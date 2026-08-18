c0c8dce016c4a44518743f72a5340f36e58032ea
Jesse Buys <jesseabuys@gmail.com>
2026-08-18 12:52:33 +0100

    feature[enhancements]: integration testing and screenshots

--- test/login_page_test.dart
+++ test/login_page_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_application_poc/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:snaptest/snaptest.dart';
 
 void main() {
   // Define a test. The TestWidgets function also provides a WidgetTester
@@ -12,16 +13,35 @@ void main() {
           home: MyLoginPage(),
         ),
       );
+
+    await tester.pumpAndSettle();
+
     await tester.enterText(
       find.byType(TextFormField).at(0),
       'JohnDoe@emails.com',
     );
+
     await tester.enterText(
       find.byType(TextFormField).at(1),
       'Password123!',
     );
+
+    await tester.pumpAndSettle();
+    
+    await snap(
+      name: 'Information in login',
+      settings: SnaptestSettings.rendered(devices: [
+        Devices.android.samsungGalaxyS20,
+      ]),
+    ); // Tap send
     await tester.tap(find.text('Log In'));
 
+    await tester.pumpAndSettle();
+    await snap(
+      settings: SnaptestSettings.rendered(devices: [
+        Devices.android.samsungGalaxyS20,
+      ]),
+    ); // Tap send
     await tester.pumpAndSettle();
     expect(find.text('Main Menu'), findsOneWidget);
     // Test code goes here.
